34f2cad69b4083e5a492cad30fd3090550892224
Ivan-PIA <wind.vanya@gmail.com>
2025-12-22 16:48:30 +0700

    int8 to int

--- include/phy/mapper.h
+++ include/phy/mapper.h
@@ -9,6 +9,6 @@
 using complex_d = std::complex<double>; 
 using complex_int16 = std::complex<int16_t>; 
 
-std::vector<complex_d> bpsk(const std::vector <int8_t> &bits);
+std::vector<complex_d> bpsk(const std::vector <int> &bits);
 
-std::vector<complex_d> qpsk(const std::vector <int8_t> &bits);
+std::vector<complex_d> qpsk(const std::vector <int> &bits);
